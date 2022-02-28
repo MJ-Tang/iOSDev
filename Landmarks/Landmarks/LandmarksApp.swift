4cf40cf3ca5058733a8d6b5401e12746d2e720ee
MJ-Tang <33923014+MJ-Tang@users.noreply.github.com>
2022-02-28 22:20:44 +0800

    create a title an image and a map uiView

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 唐明骏 on 2022/2/28.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
